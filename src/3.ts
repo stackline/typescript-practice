3098efcf1e8c8ae589ef9544811e2a9f15488f59
stackline <stackline@users.noreply.github.com>
2023-12-21 22:36:00 +0900

    add block scope to narrow scope of variables

--- src/3.ts
+++ src/3.ts
@@ -1,30 +1,32 @@
-type User = {
-  name: string;
-  age: number;
-  premiumUser: boolean;
-};
+{
+  type User = {
+    name: string;
+    age: number;
+    premiumUser: boolean;
+  };
 
-const data: string = `
+  const data: string = `
 uhyo,26,1
 John Smith,17,0
 Mary Sue,14,1
 `;
 
-const rows = data.split('\n').filter((row) => row !== '');
-const users: User[] = rows.map((row) => {
-  const [name, age, premiumUser] = row.split(',');
-  const user: User = {
-    name: name,
-    age: Number(age),
-    premiumUser: Boolean(Number(premiumUser)),
-  };
-  return user;
-});
-console.log(users);
-for (const user of users) {
-  if (user.premiumUser) {
-    console.log(`${user.name} (${user.age}) is premium user.`);
-  } else {
-    console.log(`${user.name} (${user.age}) is not premium user.`);
+  const rows = data.split('\n').filter((row) => row !== '');
+  const users: User[] = rows.map((row) => {
+    const [name, age, premiumUser] = row.split(',');
+    const user: User = {
+      name: name,
+      age: Number(age),
+      premiumUser: Boolean(Number(premiumUser)),
+    };
+    return user;
+  });
+  console.log(users);
+  for (const user of users) {
+    if (user.premiumUser) {
+      console.log(`${user.name} (${user.age}) is premium user.`);
+    } else {
+      console.log(`${user.name} (${user.age}) is not premium user.`);
+    }
   }
 }
